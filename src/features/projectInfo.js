925431e112c59eb00d1e5433f29e41ecf198c7a6
patKilcullen <patrickjkilcullen@gmail.com>
2023-07-03 17:28:11 -0500

    spelling in prject descriptions

--- src/features/projectInfo.js
+++ src/features/projectInfo.js
@@ -9,7 +9,7 @@ export const projects = [
 {
     name: "EZ-Stack",
     description: "An app that connects freelancers to clients.",
-    about: "I worked extensively with my group to build this highly functional, sleek app from start to finish. I played an important role in the ideation fo the app and its many features, I put a lot of time into the database schema, which icluded 7 models, I probably spent most of my time implementing info from the database in react components, and I aided in the site's design. ",
+    about: "I worked extensively with my group to build this highly functional, sleek app from start to finish. I played an important role in the ideation of the app's many features, I put a lot of time into the database schema, which icluded 7 models, I spent most of my time implementing info from the database in React components, and I aided in the site's design. ",
     code: "https://github.com/patKilcullen/EZ-Stack",
     demo: "https://youtu.be/4eurtRFDq0s",
     tech: "Node.js, PostgreSQL, Express, React, Redux, Material UI, CSS, Cloudinary, JWT",
@@ -18,7 +18,7 @@ export const projects = [
 {
     name: "Grace-Shopper",
     description: "The All-In-One E-Commerce Platform.",
-    about: "My first substantial group project. I spent ample time working out the authentication features of the app, and well as the cart feature, which was the first time I worked extensively with a through table.",
+    about: "My first substantial group project. I spent ample time working out the authentication features of the app, as well as the cart feature, which was the first time I worked extensively with a through table.",
     code: "https://github.com/patKilcullen/Grace-Shopper1",
     demo: "https://www.youtube.com/watch?v=lmXfjCeO7IA",
     tech: "Node.js, PostgreSQL, Express, React, Redux, Tailwind CSS, JWT",
@@ -27,7 +27,7 @@ export const projects = [
 {
     name: "Balderdash",
     description: "An interactive word game to play alone or with friends.",
-    about: "I based this app off the somewhat obscure word game also called Balderdash. Played alone, a user can see how many definition they can guess correctly out of a certain number. Played with other, one user sees the correct definition of the word, and the others have to guess. \n This app is currently under development. It was built as a challenge to see what I could build in a single weekend. It's current state was built in that time frame, but I look forward to adding to it.",
+    about: "I based this app off the somewhat obscure word game also called Balderdash. Played alone, a user can see how many definition they can guess correctly out of a certain number. Played with others, one user sees the correct definition of the word, and the others have to guess. \n This app is currently under development. It was built as a challenge to see what I could build in a single weekend. It's current state was built in that time frame, but I look forward to adding to it.",
     code: "https://github.com/patKilcullen/Balderdash",
     demo: "https://youtu.be/X0WvwwF1Ux4",
     tech: "Node.js, PostgreSQL, Express, React, Redux",
@@ -36,7 +36,7 @@ export const projects = [
 {
     name: "Full Stack App",
     description: "This was the first full stack app I ever made!",
-    about: "It may not look like much, but I was proud of my first full stap, CRUD app. I received the highest grade in my cohort of 60+ students, and my instructor described it as the 'Best project I have seen yet!' I put a lot of effort into it, and thoroughly searched for and eliminated manny bugs.",
+    about: "It may not look like much, but I was proud of my first full stack CRUD app. I received the highest grade in my cohort of 60+ students, and my instructor described it as the 'Best project I have seen yet!' I put a lot of effort into it and thoroughly searched for and eliminated many bugs.",
     code: "https://github.com/patKilcullen/jpfp-template-V2-a",
     demo: "https://youtu.be/yNKRxSyjyG8",
     tech: "Node.js, PostgreSQL, Express, React, Redux, CSS",
